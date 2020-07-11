3b2a896315b6a0edb0af4a153c3f5a4c3079c17d
taylorallen0913 <taylorallen0913@gmail.com>
2020-07-11 14:02:57 -0700

    feat: added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
