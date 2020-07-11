bf9bd72458dee2de521e9fc2c15b77e50583f291
taylorallen0913 <taylorallen0913@gmail.com>
2020-07-11 14:04:04 -0700

    style: added prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
   return (
