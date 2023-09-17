cfdcffc6712cc701ab3ba86fe15a5437448fdd93
Taylor Allen <taylorallen0913@gmail.com>
2023-09-17 12:22:41 -0700

    redeploy to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main className='mt-48'>
-      <h1 className='text-7xl text-center'>Coming Soon...</h1>
+    <main className='mt-46'>
+      <h1 className='text-7xl text-center'>Coming Soon... </h1>
     </main>
   );
 }
